6d438f943d7a84039ddeec38cb6aa30082de0697
Adam Groves <adam.groves@gmail.com>
2018-12-18 10:22:34 +0100

    elm 0.19 has a new api
    instead of seperate programs for elm-make, elm-install, elm-package etc, we now have one command to rule them all: elm.

--- config/webpack/loaders/elm.js
+++ config/webpack/loaders/elm.js
@@ -2,9 +2,9 @@ const { resolve } = require('path')
 
 const isProduction = process.env.NODE_ENV === 'production'
 const elmSource = resolve(process.cwd())
-const elmMake = `${elmSource}/node_modules/.bin/elm-make`
+const elm = `${elmSource}/node_modules/.bin/elm`
 
-const elmDefaultOptions = { cwd: elmSource, pathToMake: elmMake }
+const elmDefaultOptions = { cwd: elmSource, pathToElm: elm }
 const developmentOptions = Object.assign({}, elmDefaultOptions, {
   verbose: true,
   warn: true,

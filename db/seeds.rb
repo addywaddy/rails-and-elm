5a8b33c8695a50b99558108a3bf2b25d884fbf0a
Adam Groves <adam.groves@gmail.com>
2018-12-18 13:29:27 +0100

    Let's add some messages!
    rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+NAMES = %w( Sarah John Peter Colin Claire Helen )
+
+WORDS = %w( lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum )
+
+
+10.times do
+  Message.create!(
+    name: NAMES.sort_by { rand }.first,
+    content: WORDS.sort_by { rand }.take(10).join
+  )
+end
